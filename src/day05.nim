19a851d98f2a97ab6414ff98907088b6f163b13a
Andrea Venuta <venutawebdesign@gmail.com>
2023-12-06 00:31:34 +0100

    Day 5

--- src/day05.nim
+++ src/day05.nim
@@ -2,42 +2,6 @@ import std/algorithm
 import std/sequtils
 import std/strutils
 
-const TEST_CASE: string = """
-seeds: 79 14 55 13
-
-seed-to-soil map:
-50 98 2
-52 50 48
-
-soil-to-fertilizer map:
-0 15 37
-37 52 2
-39 0 15
-
-fertilizer-to-water map:
-49 53 8
-0 11 42
-42 0 7
-57 7 4
-
-water-to-light map:
-88 18 7
-18 25 70
-
-light-to-temperature map:
-45 77 23
-81 45 19
-68 64 13
-
-temperature-to-humidity map:
-0 69 1
-1 0 69
-
-humidity-to-location map:
-60 56 37
-56 93 4
-""".strip()
-
 #
 # Range types
 #
@@ -142,7 +106,6 @@ proc findMin(self: Maps, input: seq[Range]): int =
   for range in self.mapRanges(input):
     result = min(result, range.start)
 
-
 #
 # Parsing procs
 #
@@ -209,8 +172,3 @@ proc run1*(input: string): int =
 proc run2*(input: string): int =
   let (maps, seeds) = parseInput2(input)
   maps.findMin(seeds)
-
-when isMainModule:
-  let (maps, seeds) = parseInput2(TEST_CASE)
-
-  echo maps.findMin(seeds)
